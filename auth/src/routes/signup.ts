a5306017000e712ed8ae4dacbde5cc4b793039e1
Avishka Indula <avishkainduladev@gmail.com>
2023-10-02 06:27:00 +0530

    Creating a middleware for validate request

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,8 @@
 import express, { Request, Response } from "express";
-import { body, validationResult } from "express-validator";
-import { RequestValidationError } from "../errors/request-validation-error";
+import { body } from "express-validator";
 import { User } from "../models/user";
 import { BadRequestError } from "../errors/bad-request-error";
+import { validateRequest } from "../middlewares/validate-request";
 import jwt from "jsonwebtoken";
 
 const router = express.Router();
@@ -16,23 +16,8 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must be between 4 and 20 characters"),
   ],
-
+  validateRequest,
   async (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      // JavaScript approach on handling errors
-      // const error = new Error("Invalid email or password");
-      // error.reasons = errors.array();
-      // throw error;
-      // The reasons array will be appended to the error object and will be
-      // available in the error handler middleware.
-
-      throw new RequestValidationError(errors.array());
-      // As we've imported the express-async-errors package, we can throw errors
-      // inside of async functions without having to use next() to pass the error.
-    }
-
     const { email, password } = req.body;
 
     const existingUser = await User.findOne({ email });
