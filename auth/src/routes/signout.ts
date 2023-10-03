4cdb803b4de32dddffb853fab1fc3f8e59486e9b
Avishka Indula <avishkainduladev@gmail.com>
2023-10-03 06:41:53 +0530

    creating current-user and signout routes

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,11 @@ import express from "express";
 const router = express.Router();
 
 router.post("/api/users/signout", (req, res) => {
-  res.send("Hi there!");
+  req.session = null;
+  // This will delete the cookie, hence signing out the user
+  // This is mentioned in the cookie-session documentation
+
+  res.send({});
 });
 
 export { router as signoutRouter };
