9a50a1be5a4e24b34deed8e3d8ae621c909cbcb9
Avishka Indula <avishkainduladev@gmail.com>
2023-10-03 07:22:18 +0530

    creating require-auth and current-user middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,21 +1,13 @@
 import express from "express";
-import jwt from "jsonwebtoken";
+
+import { currentUser } from "../middlewares/current-user";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req, res) => {
-  // if (!req.session?.jwt) { is the same as if (!req.session || !req.session.jwt)
-  if (!req.session?.jwt) {
-    return res.send({ currentUser: null });
-  }
-
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    res.send({ currentUser: payload });
-  } catch (err) {
-    res.send({ currentUser: null });
-    // This will send currentUser null if the token is invalid
-  }
+router.get("/api/users/currentuser", currentUser, (req, res) => {
+  res.send({ currentUser: req.currentUser || null });
+  // We add || null because req.currentUser can be undefined.
+  // We should not send undefined as a response, instead we should send null.
 });
 
 export { router as currentUserRouter };
