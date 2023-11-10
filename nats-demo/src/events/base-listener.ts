e856cac5bdb691f053cb671ec687133426b03ebe
Avishka Indula <avishkainduladev@gmail.com>
2023-11-10 08:09:45 +0530

    Refactor base-listener and ticket-created-listener classes

--- nats-demo/src/events/base-listener.ts
+++ nats-demo/src/events/base-listener.ts
@@ -1,22 +1,27 @@
 import { Message, Stan } from "node-nats-streaming";
+import { Subjects } from "./subjects";
 
-export abstract class Listener {
-  // We don't define abstract methods inside an abstract class. We just define the method signature.
-  // We will define the method body in the child class. So what we define in abstract class is
-  //  more like a blueprint for the child class.
-  // Abstract properties are also defined in the same way.
-  abstract subject: string;
+interface Event {
+  subject: Subjects;
+  data: any;
+  // Here we are using the any type for the data property because we don't know
+  // what type of data will be published with the event. We will define the
+  // structure of the data in the interface of the child class event.
+}
+
+export abstract class Listener<T extends Event> {
+  // <T extends Event> is a generic type constraint. It is a way to tell TS that
+  // the type of the subject property defined in classes that extends Listener will always 
+  // be the same as the type of the subject property defined in the Event interface.
+  abstract subject: T["subject"];
   abstract queueGroupName: string;
-  abstract onMessage(data: any, msg: Message): void;
-  // This is the callback function that will be executed when a message is received from the publisher
+  abstract onMessage(data: T["data"], msg: Message): void;
   private client: Stan;
   protected ackWait = 5 * 1000;
 
   constructor(client: Stan) {
     this.client = client;
   }
-  // This constructor will be called when we create a new instance of the listener class. We will pass the client
-  // to the constructor.
 
   subscriptionOptions() {
     return this.client
@@ -24,18 +29,15 @@ export abstract class Listener {
       .setManualAckMode(true)
       .setAckWait(this.ackWait)
       .setDurableName(this.queueGroupName);
-    // We can set some options for the subscription. We can use the subscriptionOptions() method to set the options.
   }
 
   parseMessage(msg: Message) {
     const data = msg.getData();
-    // msg.getData() will return the data that was published by the publisher
 
     return typeof data === "string"
       ? JSON.parse(data)
       : JSON.parse(data.toString("utf8"));
   }
-  // This method will parse the message and return the parsed data
 
   listen() {
     const subscription = this.client.subscribe(
@@ -52,5 +54,4 @@ export abstract class Listener {
       this.onMessage(parsedData, msg);
     });
   }
-  // This listen() method is the main method that will be called when we create a new instance of the listener class.
 }
--- nats-demo/src/events/ticket-created-listener.ts
+++ nats-demo/src/events/ticket-created-listener.ts
@@ -1,15 +1,32 @@
 import { Message } from "node-nats-streaming";
 import { Listener } from "./base-listener";
+import { TicketCreatedEvent } from "./ticket-created-event";
+import { Subjects } from "./subjects";
 
-export class TicketCreatedListener extends Listener {
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   // We can extend the listener class to create a new listener class. And the abstract methods and properties
   // should be defined in the child class.
-  subject = "ticket:created";
+  // <TicketCreatedEvent> is a generic type constraint. As we've seen in the base-listener.ts file, the
+  // subject property is of type T["subject"]. So we need to tell TS that the type of the subject property
+  // defined in the TicketCreatedListener class will always be the same as the type of the subject property
+  // defined in the TicketCreatedEvent interface.
+
+  // subject = "ticket:created";
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
+  // We need to add a type annotation to the subject property because typescript thinks that we might try
+  // to change the value of the subject property with a wrong type of value in the future.
+
+  // readonly subject = Subjects.TicketCreated;
+  // We can also use the readonly keyword to tell TS that we will never change the value of the subject property.
 
   queueGroupName = "payments-service";
 
-  onMessage(data: any, msg: Message) {
+  onMessage(data: TicketCreatedEvent["data"], msg: Message) {
     console.log("Event data!", data);
+
+    // console.log(data.name);
+    // console.log(data.cost);
+    // The above will throw errors as data property onTicketCreatedEvent doesn't have name and cost properties.
 
     msg.ack();
   }
